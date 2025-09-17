ceaa90d89f5b11848fface8c04581bc0e590632d
Juliette Suc <juliette.suc@outlook.fr>
2025-09-17 14:10:51 +0200

    fix: commit client folder

---
+++ client/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="">
+      <main className="flex flex-col justify-center items-center h-100">
+        <h1 className="p-3">Test de titre</h1>
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Tester
+        </button>
+      </main>
+    </div>
+  );
+}
