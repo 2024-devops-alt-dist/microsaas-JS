7b5ea69994a9de373ba47c15245ccdf1b89362a3
Juliette Suc <juliette.suc@outlook.fr>
2025-09-17 14:20:43 +0200

    fix: eslint for whole project

--- api/app.ts
+++ api/app.ts
@@ -1,14 +1,16 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (request: Request, response: Response) => { 
+app.get("/", (request: Request, response: Response) => {
   response.status(200).send("Hello World");
-}); 
-
-app.listen(PORT, () => { 
-  console.log("Server running at PORT: ", PORT); 
-}).on("error", (error) => {
-  throw new Error(error.message);
 });
+
+app
+  .listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+  })
+  .on("error", (error) => {
+    throw new Error(error.message);
+  });
