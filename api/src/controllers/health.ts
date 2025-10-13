76d41844994b5743ef453d950686f7003e081e5d
Juliette Suc <juliette.suc@outlook.fr>
2025-10-13 10:14:45 +0200

    fix: docker starting bug now resolved

--- api/src/controllers/health.ts
+++ api/src/controllers/health.ts
@@ -4,7 +4,7 @@ import { pool } from "../db/config";
 export const healthController = {
   getAll: async (req: Request, res: Response) => {
     try {
-      const data = await pool.query("SELECT * FROM beer");
+      const data = await pool.query("SELECT * FROM test");
       res.status(200).json({ data: data.rows });
     } catch (error) {
       res.status(200).json({ msg: error, message: "y a une erreur" });
