7529facd79d44593106cffccd47a98f966f5b729
Juliette Suc <juliette.suc@outlook.fr>
2026-01-23 09:54:13 +0100

    feat: working timeout for playwright tests

--- client/tests/homePage..spec.ts
+++ client/tests/homePage..spec.ts
@@ -3,9 +3,8 @@ import { test, expect } from "@playwright/test";
 test("Home page has title", async ({ page }) => {
   await page.goto("http://localhost:5173/");
 
-  // Expect the main heading
-  const heading = page.getByRole("heading", {
-    name: "Surprise ! Des cadeaux bien gérés",
-  });
-  await expect(heading).toBeVisible();
+  await expect(page.locator("h1")).toHaveText(
+    "Surprise ! Des cadeaux bien gérés",
+    { timeout: 7000 },
+  );
 });
